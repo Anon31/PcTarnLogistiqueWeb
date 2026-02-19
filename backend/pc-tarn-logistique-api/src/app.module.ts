f983efc1cd3997d7aeb55122b58bd792efab86d5
ogene70 <ogene.joseph@etu.iut-tlse3.fr>
2026-02-19 09:22:01 +0100

    chore(file):appModule resolve conflict import

--- backend/pc-tarn-logistique-api/src/app.module.ts
+++ backend/pc-tarn-logistique-api/src/app.module.ts
@@ -1,12 +1,10 @@
 import { EnumsModule } from './modules/enums/enums.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
-
 import { VehiculesController } from './vehicules/vehicules.controller';
 import { VehiculesModule } from './vehicules/vehicules.module';
 import { VehiculesService } from './vehicules/vehicules.service';
 import { LotsModule } from './lots/lots.module';
-
 import { PrismaService } from './prisma/prisma.service';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
