f19d3085408ebbc80c3c1d9f645c2860d84ed180
Anon31 <anon31@protonmail.com>
2026-02-21 22:52:10 +0100

    fix(spec): mise à jour des fichiers spec backend frontend

--- frontend/pc-tarn-logistique-web/src/app/views/components/tables/table-user/table-user.component.spec.ts
+++ frontend/pc-tarn-logistique-web/src/app/views/components/tables/table-user/table-user.component.spec.ts
@@ -1,23 +1,25 @@
+import { ToasterService } from '../../../../core/services/toaster.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TableUserComponent } from './table-user.component';
+import { MessageService } from 'primeng/api';
 
 describe('TableUserComponent', () => {
-  let component: TableUserComponent;
-  let fixture: ComponentFixture<TableUserComponent>;
+    let component: TableUserComponent;
+    let fixture: ComponentFixture<TableUserComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TableUserComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TableUserComponent],
+            providers: [ToasterService, MessageService, provideHttpClientTesting()],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(TableUserComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+        fixture = TestBed.createComponent(TableUserComponent);
+        component = fixture.componentInstance;
+        await fixture.whenStable();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- frontend/pc-tarn-logistique-web/src/app/views/pages/user/user-list/user-list.component.spec.ts
+++ frontend/pc-tarn-logistique-web/src/app/views/pages/user/user-list/user-list.component.spec.ts
@@ -1,23 +1,25 @@
+import { ToasterService } from '../../../../core/services/toaster.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserListComponent } from './user-list.component';
+import { MessageService } from 'primeng/api';
 
 describe('UserListComponent', () => {
-  let component: UserListComponent;
-  let fixture: ComponentFixture<UserListComponent>;
+    let component: UserListComponent;
+    let fixture: ComponentFixture<UserListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [UserListComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [UserListComponent],
+            providers: [ToasterService, MessageService, provideHttpClientTesting()],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(UserListComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+        fixture = TestBed.createComponent(UserListComponent);
+        component = fixture.componentInstance;
+        await fixture.whenStable();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
