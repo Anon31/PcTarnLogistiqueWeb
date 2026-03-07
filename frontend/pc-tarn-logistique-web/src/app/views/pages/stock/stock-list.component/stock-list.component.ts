6fb4b0e83de028a8a0bb8b77dc7030e9fbafae6a
Shrod18 <pauline.bertrand.461@gmail.com>
2026-03-07 11:09:49 +0100

    feat(stock): Ajout du tableau stock

--- frontend/pc-tarn-logistique-web/src/app/views/pages/stock/stock-list.component/stock-list.component.ts
+++ frontend/pc-tarn-logistique-web/src/app/views/pages/stock/stock-list.component/stock-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { PageCardWrapperComponent } from "../../../../shared/components/page-card-wrapper/page-card-wrapper.component";
+import { PageCardWrapperComponent } from '../../../../shared/components/page-card-wrapper/page-card-wrapper.component';
+import { TableStockComponent } from '../../../components/tables/table-stock/table-stock.component';
 
 @Component({
-    selector: 'app-stock-list.component',
-    imports: [PageCardWrapperComponent],
+    selector: 'app-stock-list',
+    standalone: true,
+    imports: [PageCardWrapperComponent, TableStockComponent],
     templateUrl: './stock-list.component.html',
     styleUrl: './stock-list.component.css',
 })
