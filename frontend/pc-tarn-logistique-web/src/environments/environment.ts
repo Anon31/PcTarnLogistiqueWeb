5ef9b06da54a7548c0d58d1a51eb37bd22430af2
Anon31 <anon31@protonmail.com>
2026-03-30 18:59:30 +0200

    cicd(docker): mise à jour des fichiers environment.ts et environment.prod.ts

--- frontend/pc-tarn-logistique-web/src/environments/environment.ts
+++ frontend/pc-tarn-logistique-web/src/environments/environment.ts
@@ -2,4 +2,7 @@ export const environment = {
     production: false,
     API_URL: 'http://localhost:3000/api/v1',
     IMAGE_PATH: 'assets/img',
+    ICONS_PATH: 'assets/icons',
+    SVG_PATH: 'assets/svg',
+    FONTS_PATH: 'assets/fonts',
 };
